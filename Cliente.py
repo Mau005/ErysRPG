b2a3fca07b22470c37045b8895d48b5e034748a2
Mau <mpino1701@gmail.com>
2022-02-16 05:31:52 -0300

    se agrega una variable global, para ideentificar los tamaños nuevos

--- Cliente.py
+++ Cliente.py
@@ -8,22 +8,24 @@ from kivy.app import App
 from kivy.core.window import Window
 from kivy.clock import Clock
 
+TOOLS = {"size" : Window.size}
 
 class Cliente(App):
 
     def __init__(self, **kargs):
         App.__init__(self, **kargs)
         self.manejador = ScreenManager()
-        self.juego = Juego()
+        self.juego = Juego(TOOLS)
         self.manejador.add_widget(self.juego)
         Clock.schedule_interval(self.update,1/60)
         Clock.schedule_interval(self.draw,1/60)
         
     def update(self,*dt):
         self.juego.update(dt)
+        TOOLS.update({"size":Window.size})
         
     def draw(self,*dt):
-        self.juego.draw(Window.size,dt)
+        self.juego.draw(dt)
 
     def build(self):
         return self.manejador
